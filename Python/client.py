9f061701957c83c13e741d44fb35f61cc6cf642c
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-02-27 12:23:36 +0530

    Add files via upload

---
+++ Python/client.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+import os
+
+import buffer
+
+HOST = '192.168.43.192'
+PORT = 2345
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+with s:
+    sbuf = buffer.Buffer(s)
+
+    hash_type = input('Enter hash type: ')
+
+    files = input('Enter file(s) to send: ')
+    files_to_send = files.split()
+
+    for file_name in files_to_send:
+        print(file_name)
+        sbuf.put_utf8(hash_type)
+        sbuf.put_utf8(file_name)
+
+        file_size = os.path.getsize(file_name)
+        sbuf.put_utf8(str(file_size))
+
+        with open(file_name, 'rb') as f:
+            sbuf.put_bytes(f.read())
+        print('File Sent')
+        
+
