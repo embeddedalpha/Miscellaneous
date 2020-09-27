1ddbbe41cd692b28b7e6fb998d2ffe2634de94d1
Axion Engineering <kunalsalvi63@gmail.com>
2020-09-27 20:32:34 +0530

    Update writtofile.h

--- C/myfilelib/writtofile.h
+++ C/myfilelib/writtofile.h
@@ -1,7 +1,9 @@
 #ifndef _writetofile_h_
 #define _writetofile_h_
 
-
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 void Write_to_file(char *data, int data_len, char *filename, int filename_len);
 
