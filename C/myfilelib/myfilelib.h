ee14d86f017e9d97bd07f1a752162cfa93ceb637
Axion Engineering <kunalsalvi63@gmail.com>
2020-10-05 21:33:58 +0530

    Update myfilelib.h

--- C/myfilelib/myfilelib.h
+++ C/myfilelib/myfilelib.h
@@ -10,5 +10,6 @@ void Duplicate_Files(char *file_1_name,char *file_2_name);
 char * Read_File(char *filename);
 void Create_File(char *filename);
 void ListallFilesindir(char *direct);
+void Remove_Stuff_from_File(char *file1_name, char *file2_name, int character);
 
 #endif
