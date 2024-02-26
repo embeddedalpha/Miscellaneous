05145d12c95ec1c8f8fdd62bd73deeeae3e97a0b
Kunal Salvi <kunalsalvi63@gmail.com>
2024-02-25 19:35:35 -0600

    Update Swap_Integers.c

--- C Coding Q&A/Swap_Integers.c
+++ C Coding Q&A/Swap_Integers.c
@@ -1,6 +1,23 @@
+#include <stdio.h>
+#include "stdbool.h"
+
 void swap_two_integers(int *a, int *b)
 {
     *a = *a ^ *b;
     *b = *a ^ *b;
     *a = *a ^ *b;
+}
+
+int main() {
+
+int x = 155;
+int y = 98;
+printf(" before swapping: x = %d, y = %d \n",x,y);
+
+swap_two_integers(&x,&y);
+printf(" after swapping: x = %d, y = %d \n",x,y);
+
+
+
+    return 0;
 }
